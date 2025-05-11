964663c1a17a4533dd8ea55e3a756623ff7596f1
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2025-05-12 00:10:21 +0300

    optimize: UnionFind now requires less memory

--- ImageSegmentation/UnionFind.cs
+++ ImageSegmentation/UnionFind.cs
@@ -1,43 +1,30 @@
-﻿using System;
-
-namespace ImageTemplate
+﻿namespace ImageTemplate
 {
-    /* Custom UnionFind DS implemenation -- based on Alogrithms 4th edtion implementation
-       with internal difference (Int) calculations.
-
-       Internal Difference:
-        The internal difference of a component C⊆V is defined as the largest weight edge that is 
-        necessarily to keep the component connected. That is, without this edge, the component becomes 
-        disconnected. In other word, larger edges are not necessary to keep the component connected 
-        while smaller edges are necessary to keep it connected.
-
-        Int⁡(C)= Max_(ⅇ∈C)(w(e))
-    */
+    // Custom UnionFind DS implemenation -- based on Alogrithms 4th edtion implementation
     public class UnionFind
     {
-        // Storing 4 dense pixel arrays, is very memory intensive.
-        //  - `size`: could be removed, but calculating size would result in an O(n) operation
-        //  - `rank`: removing rank array would result in `Find` operation being O(nlog(n)) instead of ~O(1)
 
+        public int[] Parent { get => _parent; }
         private int[] _parent;
-        private int[] _size;
-        private int[] _rank;
 
-        // We only care abount the smallest connectivity edge weight and not the whole MST.
-        private float[] _internalDiff;
+        // removing rank array would result in `Find` operation being O(nlog(n)) instead of amortized ~O(1)
+        private int[] _rank;
 
         public UnionFind(int size)
         {
             _parent = new int[size];
             _rank = new int[size];
-            _size = new int[size];
-            _internalDiff = new float[size];
 
             for (int i = 0; i < size; i++)
-            {
                 _parent[i] = i;
-                _size[i] = 1;
-            }
+        }
+
+        public int[] FlattenParent()
+        {
+            for (int i = 0; i < _parent.Length; i++)
+                _parent[i] = Find(i);
+
+            return _parent;
         }
 
         public int Find(int p)
@@ -54,44 +41,24 @@ namespace ImageTemplate
             return p;
         }
 
-        public int Size(int p)
-        {
-            return _size[Find(p)];
-        }
-
-        public float InternalDiff(int p)
-        {
-            return _internalDiff[Find(p)];
-        }
-
-        public void Union(int p, int q, int weight)
+        public void Union(int p, int q)
         {
             int pID = Find(p);
             int qID = Find(q);
 
             if (pID == qID) return;
 
-            float newInternalDiff = Math.Max(weight, Math.Max(_internalDiff[pID], _internalDiff[qID]));
             if (_rank[pID] < _rank[qID])
             {
-                _internalDiff[qID] = newInternalDiff;
-                _size[qID] += _size[pID];
-
                 _parent[pID] = _parent[qID];
             }
             else if (_rank[pID] > _rank[qID])
             {
-                _internalDiff[pID] = newInternalDiff;
-                _size[pID] += _size[qID];
-
                 _parent[qID] = _parent[pID];
             }
             else
             {
-                _internalDiff[pID] = newInternalDiff;
-                _size[pID] += _size[qID];
                 _rank[pID]++;
-
                 _parent[qID] = _parent[pID];
             }
         }
