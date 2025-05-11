aa85f256ab0386be444200a85f71ccd7ea3ce440
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2025-05-12 01:31:08 +0300

    feat: add interactive Click-to-Merge

--- ImageSegmentation/MainApplication.cs
+++ ImageSegmentation/MainApplication.cs
@@ -16,6 +16,7 @@ namespace ImageTemplate
             Gauss_Filter,
             Solid_Color,
             Blend_Image,
+            Selected_Regions,
         }
 
         private Dictionary<ViewMode, string> _viewModeStr = new Dictionary<ViewMode, string>
@@ -24,6 +25,7 @@ namespace ImageTemplate
             { ViewMode.Gauss_Filter, "Gauss Filter" },
             { ViewMode.Solid_Color, "Solid Color" },
             { ViewMode.Blend_Image, "Blend Image" },
+            { ViewMode.Selected_Regions, "Selected Regions" },
         };
 
         private Dictionary<ViewMode, ColorChannel> _viewModeChannel = new Dictionary<ViewMode, ColorChannel>()
@@ -32,10 +34,17 @@ namespace ImageTemplate
             { ViewMode.Gauss_Filter, ColorChannel.All },
             { ViewMode.Solid_Color, ColorChannel.All },
             { ViewMode.Blend_Image, ColorChannel.All },
+            { ViewMode.Selected_Regions, ColorChannel.All },
         };
 
         private RGBPixel[,] _originalImage;
         private RGBPixel[,] _solidColorSegmentedImage;
+
+        private readonly Dictionary<int, Color> _segmentColorMap = new Dictionary<int, Color>();
+
+        private int[] _imageSegments;
+        private int _imageWidth, _imageHeight;
+        private readonly HashSet<int> _selectedSegments = new HashSet<int>();
 
         private ComboBox[] _comboViews = new ComboBox[2];
         private PictureBox[] _renderPanels = new PictureBox[2];
@@ -69,6 +78,20 @@ namespace ImageTemplate
             _comboViews[1].SelectedIndex = (int)ViewMode.Gauss_Filter;
         }
 
+        private void ClearUI()
+        {
+            tboxFilePath.Clear();
+            tboxSize.Clear();
+            tboxElapsedTime.Clear();
+            
+            // Not a UI element, but needs be cleared with the UI
+            _selectedSegments.Clear();
+            _segmentColorMap.Clear();
+
+            foreach (PictureBox pictureBox in _renderPanels)
+                pictureBox.Image = null;
+        }
+
         private void isGauss_CheckedChanged(object sender, EventArgs e)
         {
             nudMaskSize.Enabled = isGauss.Checked;
@@ -77,78 +100,53 @@ namespace ImageTemplate
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            tboxFilePath.Clear();
-            tboxSize.Clear();
-
-            foreach (PictureBox pictureBox in _renderPanels)
-                pictureBox.Image = null;
-
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
+                ClearUI();
+
                 //Open the browsed image and display it
                 string OpenedFilePath = openFileDialog1.FileName;
                 _originalImage = ImageOperations.OpenImage(OpenedFilePath);
+
+                _imageWidth = ImageOperations.GetWidth(_originalImage);
+                _imageHeight = ImageOperations.GetHeight(_originalImage);
+
                 ImageOperations.DisplayImage(_originalImage, pictureBox1);
 
-                string width = ImageOperations.GetWidth(_originalImage).ToString();
-                string height = ImageOperations.GetHeight(_originalImage).ToString();
-                tboxSize.Text = $"{width} x {height}";
+                tboxSize.Text = $"{_imageWidth.ToString()} x {_imageHeight.ToString()}";
                 tboxFilePath.Text = OpenedFilePath;
+
+                _imageSegments = null;
             }
         }
 
         private void btnApply_Click(object sender, EventArgs e)
         {
+            _selectedSegments.Clear();
+            _segmentColorMap.Clear();
+
             double sigma = double.Parse(txtGaussSigma.Text);
             int maskSize = (int)nudMaskSize.Value;
             int k = int.Parse(tboxK.Text);
-            float blend = (float)(numericBlend.Value / numericBlend.Maximum);
-
-            int imageWidth = ImageOperations.GetWidth(_originalImage);
-            int imageHeight = ImageOperations.GetHeight(_originalImage);
 
             RGBPixel[,] gaussImage = ImageOperations.GaussianFilter1D(_originalImage, maskSize, sigma);
-
-            Stopwatch timer = Stopwatch.StartNew();
-            int[] segments = PixelGraphSegmentator.Segment(isGauss.Checked ? gaussImage : _originalImage, k);
-            timer.Stop();
-            tboxElapsedTime.Text = timer.ElapsedMilliseconds.ToString();
-
-            _solidColorSegmentedImage = SolidColorImage(segments, imageWidth, imageHeight);
-
-            for (int i = 0; i < _renderPanels.Length; i++)
             {
-                RGBPixel[,] renderedImage = null;
-                ViewMode mode = (ViewMode)_comboViews[i].SelectedIndex;
-                ColorChannel channel = _viewModeChannel[mode];
+                Stopwatch timer = Stopwatch.StartNew();
+                _imageSegments = PixelGraphSegmentator.Segment(isGauss.Checked ? gaussImage : _originalImage, k);
+                timer.Stop();
 
-                switch (mode)
-                {
-                    case ViewMode.Orignal_Image:
-                        renderedImage = _originalImage;
-                        break;
-                    case ViewMode.Gauss_Filter:
-                        renderedImage = gaussImage;
-                        break;
-                    case ViewMode.Solid_Color:
-                        renderedImage = _solidColorSegmentedImage;
-                        break;
-                    case ViewMode.Blend_Image:
-                        renderedImage = BlendImage(_originalImage, _solidColorSegmentedImage, blend, channel);
-                        break;
-                }
-
-                ImageOperations.DisplayImage(renderedImage, _renderPanels[i]);
+                tboxElapsedTime.Text = timer.ElapsedMilliseconds.ToString();
             }
+            _solidColorSegmentedImage = SolidColorImage(_imageSegments, _imageWidth, _imageHeight);
 
-            WriteSegements(segments);
+            RefreshRenderPanels();
+            WriteSegements(_imageSegments);
         }
 
         private RGBPixel[,] SolidColorImage(int[] segments, int width, int height)
         {
             RGBPixel[,] outImage = new RGBPixel[height, width];
-            Dictionary<int, Color> colorMap = new Dictionary<int, Color>();
             Random rand = new Random();
 
             for (int y = 0; y < height; y++)
@@ -157,14 +155,14 @@ namespace ImageTemplate
                 {
                     int segment = segments[y * width + x];
 
-                    if (!colorMap.TryGetValue(segment, out Color segmentColor))
+                    if (!_segmentColorMap.TryGetValue(segment, out Color segmentColor))
                     {
                         segmentColor = Color.FromArgb(
                             rand.Next(50, 255),  // Red
                             rand.Next(50, 255),  // Green
                             rand.Next(50, 255)   // Blue
                         );
-                        colorMap[segment] = segmentColor;
+                        _segmentColorMap[segment] = segmentColor;
                     }
 
                     outImage[y, x].red = segmentColor.R;
@@ -235,21 +233,96 @@ namespace ImageTemplate
 
         private void selectedIndexChanged(object sender, EventArgs e)
         {
+            RefreshRenderPanels();
+        }
+
+        private void btnMergeRegions_Click(object sender, EventArgs e)
+        {
+            if (_selectedSegments.Count < 2 || _imageSegments == null) return;
+
+            int mergeLabel = _selectedSegments.First();
+            for (int i = 0; i < _imageSegments.Length; i++)
+            {
+                if (_selectedSegments.Contains(_imageSegments[i]))
+                    _imageSegments[i] = mergeLabel;
+            }
+
+            _solidColorSegmentedImage = SolidColorImage(_imageSegments, _imageWidth, _imageHeight);
+            _selectedSegments.Clear();
+            //_selectedSegments.Add(mergeLabel);
+
+            RefreshRenderPanels();
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (_imageSegments == null || _imageWidth == 0 || _imageHeight == 0) return;
+
+            int idx = e.Y * _imageWidth + e.X;
+            if (idx < 0 || idx >= _imageSegments.Length) return;
+
+            int segLabel = _imageSegments[idx];
+            if (_selectedSegments.Contains(segLabel))
+                _selectedSegments.Remove(segLabel);
+            else
+                _selectedSegments.Add(segLabel);
+
+            RefreshRenderPanels();
+        }
+
+        private RGBPixel[,] HighlightSelectedRegions(RGBPixel[,] image, bool invert)
+        {
+            if (_imageSegments == null) return null;
+
+            RGBPixel[,] displayImage = (RGBPixel[,]) image.Clone();
+            for (int y = 0; y < _imageHeight; y++)
+            {
+                for (int x = 0; x < _imageWidth; x++)
+                {
+                    int idx = y * _imageWidth + x;
+
+                    if (invert)
+                    {
+                        if (!_selectedSegments.Contains(_imageSegments[idx]))
+                        {
+                            displayImage[y, x].red = 255;
+                            displayImage[y, x].green = 255;
+                            displayImage[y, x].blue = 255;
+                        }
+                    }
+                    else
+                    {
+                        if (_selectedSegments.Contains(_imageSegments[idx]))
+                        {
+                            displayImage[y, x].red = 255;
+                            displayImage[y, x].green = 255;
+                            displayImage[y, x].blue = 0;
+                        }
+                    }
+                }
+            }
+
+            return displayImage;
+        }
+
+        private void RefreshRenderPanels()
+        {
             if (_originalImage == null || _solidColorSegmentedImage == null) return;
 
             double sigma = double.Parse(txtGaussSigma.Text);
-            int maskSize = (int) nudMaskSize.Value;
+            int maskSize = (int)nudMaskSize.Value;
             float blend = (float)(numericBlend.Value / numericBlend.Maximum);
 
             for (int i = 0; i < _renderPanels.Length; i++)
             {
                 RGBPixel[,] renderedImage = null;
-                ViewMode mode = (ViewMode) _comboViews[i].SelectedIndex;
+                ViewMode mode = (ViewMode)_comboViews[i].SelectedIndex;
                 ColorChannel channel = _viewModeChannel[mode];
 
                 switch (mode)
                 {
                     case ViewMode.Orignal_Image:
+                    case ViewMode.Selected_Regions:
                         renderedImage = _originalImage;
                         break;
                     case ViewMode.Gauss_Filter:
@@ -264,6 +337,7 @@ namespace ImageTemplate
                         break;
                 }
 
+                renderedImage = HighlightSelectedRegions(renderedImage, mode == ViewMode.Selected_Regions);
                 ImageOperations.DisplayImage(renderedImage, _renderPanels[i]);
             }
         }
